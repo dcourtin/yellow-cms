8d3e6623b93bb17c07303ba53375230385b37c95
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-15 10:48:55 +0200

    Update YellowCmsServiceProvider.php

--- src/YellowCmsServiceProvider.php
+++ src/YellowCmsServiceProvider.php
@@ -15,11 +15,56 @@ class YellowCmsServiceProvider extends PackageServiceProvider
          *
          * More info: https://github.com/spatie/laravel-package-tools
          */
+        /*
         $package
             ->name('yellow-cms')
             ->hasConfigFile()
             ->hasViews()
             ->hasMigration('create_yellow-cms_table')
             ->hasCommand(YellowCmsCommand::class);
+            */
+    }
+
+    public function boot()
+    {
+
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'yellowcms');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'yellowcms');
+
+        $this->publishes([
+            __DIR__.'/../config/yellow-cms.php' => config_path('yellow-cms.php'),
+        ],'yellowcms-config');
+
+        $this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/yellowcms'),
+        ], 'yellowcms-lang');
+
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/yellowcms'),
+        ], 'yellowcms-views');
+
+        $this->publishes([
+            __DIR__.'/../public' => public_path('vendor/yellowcms'),
+        ], 'yellowcms-public');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ], 'yellowcms-migrations');
+
+        /*
+        $this->loadViewComponentsAs('yellowcms', [
+            Alert::class,
+            Button::class,
+        ]);
+         */
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/yellow-cms.php', 'yellowcms'
+        );
     }
 }
