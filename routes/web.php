8cac68c2f1ef7bf2c1d05fa4aa751651de2efd22
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-15 11:41:17 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use Dcourtin\YellowCms\Http\Controllers\NodeController;
 
-get('/{nodeType}/{page?}/index',[NodeController::class, 'nodeList'])->name('nodeList');
+Route::get('/{nodeType}/{page?}/index',[NodeController::class, 'nodeList'])->name('nodeList');
 Route::get('/{nodeType}/{nodeSlug}/show',[NodeController::class, 'nodeShow'])->name('nodeShow');
