16827eadc4013eadc22385a91d4a6e0eb03316fa
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-15 15:30:26 +0200

    Update Controller.php

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace Dcourtin\YellowCms\Http\Controllers;
 
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
