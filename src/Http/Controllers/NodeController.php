47d517e36c117b36222243f5fe50d9e29a8ab7aa
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-15 11:06:48 +0200

    Add Controllers

---
+++ src/Http/Controllers/NodeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dcourtin\YellowCms\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NodeController extends Controller
+{
+
+    public function nodeList($nodeType,$page)
+    {
+        $nodeList  = getNodeList($nodeType, $page);
+        return view("nodes/$nodeType/index" );
+    }
+
+    public function nodeShow($nodeType, $nodeSlug)
+    {
+        //la fonction node est issu du fichier app\helpers
+        return view("nodes/$nodeType/show",['node'=> getNode($nodeType,$nodeSlug)] );
+    }
+}
