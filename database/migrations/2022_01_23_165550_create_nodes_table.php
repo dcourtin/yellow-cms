b93d998f3b2f788651f7516d4c58081b15e49acb
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-15 11:51:23 +0200

    Update migrations

--- database/migrations/2022_01_23_165550_create_nodes_table.php
+++ database/migrations/2022_01_23_165550_create_nodes_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\NodeType;
+use Dcourtin\YellowCms\Models\NodeType;
 
 class CreateNodesTable extends Migration
 {
