d29eaf3c84a34d522750de0998b4071d22925b2f
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-15 16:50:37 +0200

    Update index.blade.php

--- resources/views/admin/node_type/index.blade.php
+++ resources/views/admin/node_type/index.blade.php
@@ -2,7 +2,7 @@
    <ul>
         @foreach($nodeTypes as $nodeType)
             <li class="border-b border-gray-200 py-4 hover:bg-gray-100">
-                <a href="{{route('node_type_show', ['id'=>$nodeType->id])}}">{{$nodeType->title}}</a>
+                <a href="{{route('node_type_show', ['id'=>$nodeType->id])}}">{{$nodeType->name}}</a>
             </li>
         @endforeach
     </ul>
