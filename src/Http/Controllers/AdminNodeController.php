b3608e14ca5a554be2d31967d8cfccddcc85a681
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-15 15:08:42 +0200

    Add admin controllers

--- src/Http/Controllers/AdminNodeController.php
+++ src/Http/Controllers/AdminNodeController.php
@@ -1,18 +1,128 @@
 <?php
 
-namespace Dcourtin\YellowCms\Http\Controllers;
+namespace Bdsa\YellowCms\Http\Controllers;
 
-class AdminNodeController extends Controller
+use Illuminate\Http\Request;
+use Bdsa\YellowCms\Models\Node;
+use Bdsa\YellowCms\Models\NodeType;
+use Illuminate\Support\Str;
+
+class NodeController extends Controller
 {
-    public function nodeList($nodeType, $page, $status = 'publish')
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($nodeTypeId)
     {
-        $nodeList = getNodeList($nodeType, $page);
+        $orderBy = 'created_at';
+        $order = 'DESC';
+        if($nodeTypeId){
+            $nodeQuery = Node::where(['node_type_id'=>$nodeTypeId]);
+            $nodeType  = NodeType::find($nodeTypeId);
+            if($order == 'DESC'){
+                $nodes = $nodeQuery->orderByDesc($orderBy)->get();
+            }else{
+                $nodes = $nodeQuery->orderBy($orderBy)->get();
+            }   
+        }
 
-        return view("nodes/$nodeType/index");
+        return view('node/index', [
+            'pageTitle' => isset($nodeType)?$nodeType->name:'Nodes',
+            'nodes'=>$nodes
+        ]);
     }
 
-    public function nodeShow($nodeType, $nodeSlug)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($nodeTypeId)
     {
-        return view("nodes/$nodeType/show", ['node' => getNode($nodeType, $nodeSlug)]);
+        $nodeType = NodeType::findOrFail($nodeTypeId);
+
+        return view('node/edit',
+            [
+                'pageTitle' => 'Nouveau contenu de type : '.$nodeType->name,
+                'nodeType' => $nodeType
+            ]
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if($id=$request->get('id')){
+            $node = Node::findOrFail($id);
+        }else{
+            $node = new Node();
+            $node->node_type_id = $request->get('node_type_id');
+        }
+        
+        $node->title = $request->get('title');
+        $node->slug  = $request->get('slug')??Str::slug($node->title);
+        $node->content = $request->get('content');
+        $node->status  = $request->get('status','draft');
+        $node->save();
+
+        return redirect()->route('node_show', ['id'=>$node->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {   
+        return redirect()->route('node_edit',['id'=>$id]);
+        $node = Node::findOrFail($id);
+        return view('node/show', ['node'=>$node]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $node = Node::findOrFail($id);
+        return view('node/edit', [
+            'pageTitle' => $node->title ,
+            'node'=>$node]
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
---
+++ src/Http/Controllers/AdminNodeTypeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Bdsa\YellowCms\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Bdsa\YellowCms\Models\NodeType;
+
+class NodeTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('node_type/index', ['nodeTypes' => NodeType::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('node_type/show', ['nodeType' => NodeType::find($id)]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('node_type/edit', ['nodeType' => NodeType::find($id)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return view('node_type/edit', ['nodeType' => NodeType::find($id)]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        NodeType::find($id)->delete();
+        return redirect()->route('node_type_index')->with('success','Node type deleted!');
+    }
+}
