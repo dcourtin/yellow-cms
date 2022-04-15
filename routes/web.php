b1b723be12657d08fac6f6522cae00eafa7c5633
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-15 15:13:36 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Dcourtin\YellowCms\Http\Controllers\NodeController;
+use Dcourtin\YellowCms\Http\Controllers\AdminNodeController;
+use Dcourtin\YellowCms\Http\Controllers\AdminNodeTypeController;
+use Dcourtin\YellowCms\Http\Controllers\AdminNodeFieldController;
 
 Route::get('/{nodeType}/{page?}/index',[NodeController::class, 'nodeList'])->name('nodeList');
 Route::get('/{nodeType}/{nodeSlug}/show',[NodeController::class, 'nodeShow'])->name('nodeShow');
@@ -14,14 +17,14 @@ Route::get('/admin/{nodeType}/{nodeSlug}/show',
     ->name('adminNodeShow');
 */
 
-Route::get('/admin/node_type/index', [NodeTypeController::class, 'index'])->name('node_type_index');
-Route::get('/admin/node_type/show/{id}', [NodeTypeController::class, 'show'])->name('node_type_show');
+Route::get('/admin/node_type/index', [AdminNodeTypeController::class, 'index'])->name('node_type_index');
+Route::get('/admin/node_type/show/{id}', [AdminNodeTypeController::class, 'show'])->name('node_type_show');
 
-Route::get('/admin/node_field/index', [NodeFieldController::class, 'index']);
-Route::get('/admin/node_field/show/{id}', [NodeFieldController::class, 'show']);
+Route::get('/admin/node_field/index', [AdminNodeFieldController::class, 'index']);
+Route::get('/admin/node_field/show/{id}', [AdminNodeFieldController::class, 'show']);
 
-Route::get('/admin/node/index/{node_type_id}', [NodeController::class, 'index'])->name('node_index');
-Route::get('/admin/node/show/{id}', [NodeController::class, 'show'])->name('node_show');
-Route::get('/admin/node/edit/{id}', [NodeController::class, 'edit'])->name('node_edit');
+Route::get('/admin/node/index/{node_type_id}', [AdminNodeController::class, 'index'])->name('node_index');
+Route::get('/admin/node/show/{id}', [AdminNodeController::class, 'show'])->name('node_show');
+Route::get('/admin/node/edit/{id}', [AdminNodeController::class, 'edit'])->name('node_edit');
 Route::get('/admin/node/create/{node_type_id}', [NodeController::class, 'create'])->name('node_create');
-Route::post('/admin/node/store', [NodeController::class,'store'])->name('node_store');
+Route::post('/admin/node/store', [AdminNodeController::class,'store'])->name('node_store');
