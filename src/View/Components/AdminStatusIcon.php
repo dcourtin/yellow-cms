0416221a931a880f63440708a81531751034b036
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-20 09:37:54 +0200

    Create AdminStatusIcon.php

---
+++ src/View/Components/AdminStatusIcon.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dcourtin\YellowCms\View\Components;
+
+use Illuminate\View\Component;
+
+class AdminStatusIcon extends Component
+{
+   public $status;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($status)
+    {
+
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view(
+            'yellowcms::components.admin-status-icon',
+            [
+
+            ]
+        );
+    }
+}
