de58843535421ced13d707a24728287dd0bc2ce1
Courtin David <d.courtin@bdsa-lagence.com>
2022-04-25 17:42:51 +0200

    Create Lang.php

---
+++ src/Models/Lang.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dcourtin\YellowCms\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lang extends Model
+{
+    use HasFactory;
+
+
+}
